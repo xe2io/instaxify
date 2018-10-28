61631ce789246ad7d341734979eec3cbddaff8f0
Jonathan Chen <force@xe2>
2018-10-28 15:39:21 -0500

    bash and ImageMagick based conversion

---
+++ auto_instaxify.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(readlink -e $(dirname $0))
+INPUT_DIR="input"
+INSTAXIFY="$SCRIPT_DIR/instaxify_this.sh"
+
+inotifywait --format '%f' -m -e close_write $INPUT_DIR | while read FILE
+do
+    $INSTAXIFY $FILE
+done
